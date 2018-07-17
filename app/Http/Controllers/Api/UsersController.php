e28ce4883411ffd4b774b3c14e6e9d9c7c44345d
vagrant <vagrant@homestead>
2018-07-17 07:15:03 +0000

    api/user

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -5,18 +5,22 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\UserRequest;
+use App\Transformers\UserTransformer;
 
 class UsersController extends Controller
 {
-    public function store(UserRequest $request){
+    public function store(UserRequest $request)
+    {
 
     	$verifyData = \Cache::get($request->verification_key);
 
-    	if(!$verifyData){
+    	if(!$verifyData)
+        {
     		return $this->response->error('验证码已失效',422);
     	}
 
-    	if(!hash_equals($verifyData['code'],$request->verification_code)){
+    	if(!hash_equals($verifyData['code'],$request->verification_code))
+        {
     		return $this->response->errorUnauthorized('验证码错误');
     	}
 
@@ -29,7 +33,18 @@ class UsersController extends Controller
 
     	\Cache::forget($request->verification_key);
 
-    	return $this->response->created();
+    	return $this->response->item($user,new UserTransformer())
+                    ->setMeta([
+                        'access_token' => \Auth::guard('api')->fromUser($user),
+                        'token_type' => 'Bearer',
+                        'expires_in' => \Auth::guard('api')->factory()->getTTL()*60,
+                    ])
+                    ->setStatusCode(201);
 
+    }
+
+    public function me()
+    {
+        return $this->response->item($this->user(),new UserTransformer());
     }
 }
